11d65f2fb26bbc2b10c45b7d8ccfbc6d58e77324
Lee-ndiwe <charmainemohlakoane@gmail.com>
2023-10-17 05:40:36 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+int _puts(char *str);
+int print_rev(char *s);
+int rev_strings(char *s);
+int puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+int *_strcpy(char *dest, char *src);
+void _atoi(char *s);
+
+#endif
